7f86bb192ba84533b22646a79e371221c5cdacb2
ElenMuradyan <muradyanelen0701@gmail.com>
2025-05-04 23:45:22 +0400

    feat: create main page using Aceternity UI and next-themes for dark mode support

---
+++ src/components/ui/DotBackgroundDemo.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { TextGenerateEffect } from "./TextGenerateEffect ";
+import MagicButton from "./MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+
+export function DotBackgroundDemo() {
+  return (
+    <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+        )}
+      />
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"/>
+      <div className='flex justify-center relative my-20 z-10'>
+          <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+            Hello, I'm Elen — a Frontend Developer.
+            </h2>
+            <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl" 
+            words="Transforming consepts into Seamless Experiences"/>
+
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            I craft modern, responsive, and user-friendly web experiences using technologies like React, Next.js, TypeScript and Tailwind CSS. I'm passionate about clean code, great design, and building interfaces that users love.
+            </p>
+            <a href="/about">
+            <MagicButton 
+            title="Show my work"
+            icon={<FaLocationArrow />}
+            position="right"
+            />
+            </a>
+          </div>
+        </div>
+    </div>
+  );
+}
---
+++ src/components/ui/MagicButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface MagicButtonProps {
+  title: string;
+  icon: React.ReactNode;
+  position: 'left' | 'right';
+  handleClick?: () => void;
+  otherClasses?: string;
+}
+
+const MagicButton: React.FC<MagicButtonProps> = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  otherClasses = '',
+}) => {
+  return (
+    <button
+      onClick={handleClick} style={{borderRadius: 10, marginTop: 10}}
+      className='relative rounded-lg flex items-center justify-center h-12 w-full overflow-hidden p-[1px] focus:outline-none md:w-60 md:mt-10'
+    >
+      <span style={{borderRadius: 15}} className="absolute rounded-lg inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span style={{borderRadius: 10, padding:10}} className={`inline-flex h-[97%] w-[98%] cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}`}>
+        {position === 'left' && icon}
+        {title}
+        {position === 'right' && icon}
+      </span>
+    </button>
+  );
+};
+
+export default MagicButton;
