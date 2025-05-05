58f8a29926df02d716ddaaeccff4eb5ffded77b5
ElenMuradyan <muradyanelen0701@gmail.com>
2025-05-05 23:21:51 +0400

    feat: create responsive About section with custom grid layout and updated content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { navbarItems } from "@/lib/constants";
 
 export default function Home() {
   return (
     <main className="relative bg-[#000319] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="w-full">
         <h1>
+          <FloatingNav navItems={navbarItems}/>
           <Hero />
+          <Grid />
         </h1>
       </div>
     </main>
--- src/components/ui/DotBackgroundDemo.tsx
+++ src/components/ui/DotBackgroundDemo.tsx
@@ -6,7 +6,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 export function DotBackgroundDemo() {
   return (
-    <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+    <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-[#000319]">
       <div
         className={cn(
           "absolute inset-0",
@@ -15,7 +15,7 @@ export function DotBackgroundDemo() {
           "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
         )}
       />
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"/>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-[#000319]"/>
       <div className='flex justify-center relative my-20 z-10'>
           <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
             <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
